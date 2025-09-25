e0e7a061b282c6cbab1c37f0e69b610a191acef4
LONG LA <baolonguit@gmail.com>
2025-09-24 22:20:40 -0700

    style: Refine skeleton loader widths for realism

--- src/components/BreweryListSkeleton.vue
+++ src/components/BreweryListSkeleton.vue
@@ -10,9 +10,9 @@ defineProps({
 <template>
   <div class="w-full space-y-2 animate-pulse">
     <div v-for="n in count" :key="n" class="p-4 rounded-md">
-      <div class="h-5 bg-gray-300 dark:bg-gray-700 rounded w-3/4 mb-2"></div>
-      <div class="h-4 bg-gray-300 dark:bg-gray-700 rounded w-1/2 mb-2"></div>
-      <div class="h-3 bg-gray-300 dark:bg-gray-700 rounded w-1/4"></div>
+      <div class="h-5 bg-gray-300 dark:bg-gray-700 rounded w-3/5 mb-2"></div>
+      <div class="h-4 bg-gray-300 dark:bg-gray-700 rounded w-5/12 mb-2"></div>
+      <div class="h-3 bg-gray-300 dark:bg-gray-700 rounded w-1/5"></div>
     </div>
   </div>
 </template>
