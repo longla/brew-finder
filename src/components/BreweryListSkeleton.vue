8ce4877d5f0d6968947f31e88693e0e86b9bde48
LONG LA <baolonguit@gmail.com>
2025-09-24 22:25:52 -0700

    enhance skeleton styles

--- src/components/BreweryListSkeleton.vue
+++ src/components/BreweryListSkeleton.vue
@@ -4,15 +4,19 @@ defineProps({
     type: Number,
     default: 10,
   },
-})
+});
 </script>
 
 <template>
-  <div class="w-full space-y-2 animate-pulse">
+  <div class="space-y-2 animate-pulse">
     <div v-for="n in count" :key="n" class="p-4 rounded-md">
-      <div class="h-5 bg-gray-300 dark:bg-gray-700 rounded w-3/5 mb-2"></div>
-      <div class="h-4 bg-gray-300 dark:bg-gray-700 rounded w-5/12 mb-2"></div>
-      <div class="h-3 bg-gray-300 dark:bg-gray-700 rounded w-1/5"></div>
+      <div
+        class="h-5 bg-gray-300 dark:bg-gray-700 rounded w-[250px] mb-2"
+      ></div>
+      <div
+        class="h-4 bg-gray-300 dark:bg-gray-700 rounded w-[200px] mb-2"
+      ></div>
+      <div class="h-3 bg-gray-300 dark:bg-gray-700 rounded w-[100px]"></div>
     </div>
   </div>
 </template>
