e613940d4b589ac85c11ab9baa4f3bc0d31cbaaf
LONG LA <baolonguit@gmail.com>
2025-09-24 17:47:53 -0700

    fix(router): Use createWebHistory to enable URL updates

--- src/router.ts
+++ src/router.ts
@@ -1,11 +1,11 @@
-import { createMemoryHistory, createRouter } from "vue-router";
+import { createRouter, createWebHistory } from "vue-router";
 
 import HomePage from "~/pages/HomePage.vue";
 
 const routes = [{ path: "/", component: HomePage }];
 
 const router = createRouter({
-  history: createMemoryHistory(),
+  history: createWebHistory(),
   routes,
 });
 
