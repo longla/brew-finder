cd9cc50ed3e9205ef934b6dc5ab40608812b08fa
LONG LA <baolonguit@gmail.com>
2025-09-24 22:12:28 -0700

    fix(ui): Ensure skeleton list is full-width

--- src/components/BreweryListSkeleton.vue
+++ src/components/BreweryListSkeleton.vue
@@ -8,7 +8,7 @@ defineProps({
 </script>
 
 <template>
-  <div class="space-y-2 animate-pulse">
+  <div class="w-full space-y-2 animate-pulse">
     <div v-for="n in count" :key="n" class="p-4 rounded-md">
       <div class="h-5 bg-gray-300 dark:bg-gray-700 rounded w-3/4 mb-2"></div>
       <div class="h-4 bg-gray-300 dark:bg-gray-700 rounded w-1/2 mb-2"></div>
